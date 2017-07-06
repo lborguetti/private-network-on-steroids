01def503bb905470801677548192520ee72911b4
Luciano Antonio Borguetti Faustino <lucianoborguetti@gmail.com>
2017-07-06 10:59:41 -0300

    Imported klb and map functions and fixed ssh key

--- lib/droplet.sh
+++ lib/droplet.sh
@@ -1,3 +1,7 @@
+import klb/digitalocean/ssh_key
+import klb/digitalocean/droplet
+import lib/map
+
 fn lib_droplet_create(cfg) {
 	# create instance of droplet
 	name    <= lib_map_get($cfg, "name")
@@ -8,11 +12,11 @@ fn lib_droplet_create(cfg) {
 
 	# check if ssh-key exists
 	ssh_key <= lib_map_get($cfg, "ssh_key")
-	key     <= digitalocean_ssh_key_existis($ssh_key)
+	key     <= digitalocean_ssh_key_exists($ssh_key)
 
 	if len($key) != "0" {
 		public_key_file <= lib_map_get($cfg, "public_key_file")
-		
+
 		# import ssh-key file
 		digitalocean_ssh_key_import($ssh_key, $public_key_file)
 	}
