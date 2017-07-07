0134b94f454785816283c8874e9bf78d0069de87
Vitor Arins <vitorarins@gmail.com>
2017-07-07 00:15:34 -0300

    Refactor lib droplet to return some info; Add haproxy build script; Refactor haproxy cloud-config template to use new variables; Refactor lib template to accept different keys.

--- lib/droplet.sh
+++ lib/droplet.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env nash
+
 import klb/digitalocean/ssh_key
 import klb/digitalocean/droplet
 import ./lib/map
@@ -13,22 +14,34 @@ fn lib_droplet_create(cfg) {
 
 	# check if ssh-key exists
 	ssh_key <= lib_map_get($cfg, "ssh_key")
-	key     <= digitalocean_ssh_key_exists($ssh_key)
+	key_id  <= digitalocean_ssh_key_exists($ssh_key)
 
-	if $key != "0" {
+	if $key_id == "" {
 		ssh_key_file <= lib_map_get($cfg, "ssh_key_file")
 
 		# import ssh-key file
-		digitalocean_ssh_key_import($ssh_key, $ssh_key_file)
+		key_info, err <= digitalocean_ssh_key_import($ssh_key, $ssh_key_file)
+
+		if $err != "" {
+			print("Could not import ssh key: %s", $err)
+			exit("1")
+		}
+
+		key_id <= echo $key_info | jq ".[].id"
 	}
 
-	droplet <= digitalocean_droplet_set_ssh_key($droplet, $ssh_key)
+	droplet <= digitalocean_droplet_set_ssh_key($droplet, $key_id)
 
 	# set droplet user data
 	user_data <= lib_map_get($cfg, "user_data")
 	droplet   <= digitalocean_droplet_set_user_data_file($droplet, $user_data)
 
 	# create droplet
-	droplet_info <= digitalocean_droplet_create($droplet)
-	droplet_ip   <= echo $droplet_info | jq ""
+	droplet_info, err <= digitalocean_droplet_create($droplet)
+
+	if $err != "" {
+		print("Could not create droplet: %s", $err)
+
+		return "", $err
+	}
 }
